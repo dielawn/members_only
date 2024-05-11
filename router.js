0d7900f9eebb59c0ff6d77d22e33d9fac55d7e7e
dielawn <dielawnmerkill@gmail.com>
2024-05-11 09:19:35 -0600

    added try catch to register post route

--- router.js
+++ router.js
@@ -39,31 +39,36 @@ router.post('/register',
             return true;
         }),
 
-async (req, res, next) => {
-    const errors = validationResult(req);
+    async (req, res, next) => {
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+            
+        try {
+            const hash = await bcrypt.hash(req.body.password, 10);
         
-    const hash = await bcrypt.hash(req.body.password, 10);
-   
-    const newUser = new User({
-        username: req.body.username,
-        password: hash,
-        admin: true,
-    });
-
-    // save new user to database
-    newUser.save()
-    .then((user) => {
-        console.log(`User saved: ${user}`);
-        res.redirect('/login');
-    })
-    .catch((err) => {
-        console.error(`Error saving user: ${err}`);
-        res.status(500).send('Internal Server Error');
-    });
- });
+            const newUser = new User({
+                username: req.body.username,
+                password: hash,
+                admin: true,
+            });
+            
+            // save new user to database
+            newUser.save()
+                .then((user) => {
+                    console.log(`User saved: ${user}`);
+                    res.redirect('/login');
+                })
+                .catch((err) => {
+                    console.error(`Error saving user: ${err}`);
+                    res.status(500).send('Internal Server Error');
+                });
+        } catch (error) {
+            console.error(`Error hashing password: ${error}`)
+            res.status(500).send('Internal Server Error')
+        }
+});
 
  // protected route
  router.get(`/user`, authMiddleware, (req, res) => {
